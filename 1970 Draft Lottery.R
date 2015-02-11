05b6b2c038019c3d69235147cd9aec9c4a52cda6
karapa <solon@Solons-MacBook-Pro.local>
2015-02-11 20:43:48 +0100

    ReadMe

--- 1970 Draft Lottery.R
+++ 1970 Draft Lottery.R
@@ -16,7 +16,7 @@ plot1+ geom_point (shape=1) +ggtitle("Scatterplot of the days of the year\n and 
 #create a new column in order to plot the months chronoloically 
 months<-factor(Month, levels=Month)
 
-#boxplot of Day of Year vs Draft by Month
+#boxplot of Day of Year vs Draft by Month     ##show the mean in the boxs!!!!!!
 plot2<-ggplot(mydata, aes(x=months, Draft_No.), y=Draft_No.) + geom_boxplot()
 + ggtitle("Boxplots of the months and their lottery(draft) number")
 + scale_x_discrete (name = "Month of year (Jan-Dec)")
@@ -25,7 +25,7 @@ plot2<-ggplot(mydata, aes(x=months, Draft_No.), y=Draft_No.) + geom_boxplot()
 
 ## Non-parametric regression
 
-# Local linear regression
+# Local linear regression with 3 diffrerent spans
 local_linear1<-loess(Draft_No. ~ Day_of_year, span=2/3, degree=1, data=mydata)
 local_linear2<-loess(Draft_No. ~ Day_of_year, span=1/3, degree=1, data=mydata)
 local_linear3<-loess(Draft_No. ~ Day_of_year, span=1/10, degree=1,data=mydata)
@@ -35,7 +35,7 @@ colors <- c("red", "green", "blue")
 for (i in 1:length(s)) lines(Day_of_year, predict(loess(Draft_No. ~ Day_of_year, span=s[i], degree=1), data=mydata), col=colors[i])
 legend("topright", c("span = 2/3", "span = 1/3", "span = 1/10"), lty=1, col=c("red", "green", "blue"))
 
- # Local quadratic regression
+ # Local quadratic regression with 3 different spans 
 local_quad1<-loess(Draft_No. ~ Day_of_year, span=2/3, degree=2, data=mydata)
 local_quad2<-loess(Draft_No. ~ Day_of_year, span=1/3, degree=2, data=mydata)
 local_quad3<-loess(Draft_No. ~ Day_of_year, span=1/10, degree=2, data=mydata)
@@ -57,6 +57,18 @@ autoloess <- function(fit, span=c(.1, .9)) {
 	 f <- function(span) aicc.loess(update(fit, span=span))
 	 return(update(fit, span=optimize(f, span)$minimum))
 	 }
+	 
+# Non- parametric regression based on AICc
+basic<-loess(Draft_No. ~ Day_of_year)
+loess3 <- autoloess(basic)
+plot(Day_of_year, Draft_No., xlab= "Day of the year", ylab="Draft Number", main="Non parametric regression based on AICc")
+lines(Day_of_year, loess3$fit, col=35)
 
+# Confidence bands
+predict1<-predict(loess3 , se=T)
+plot(Day_of_year, Draft_No., xlab="Day of the year ", ylab="Draft Number", main="Scatterplot of the days of the year and their lottery(draft) number\n with non-parametric regression and confidence band")
+lines(Day_of_year,predict1$fit, col=35)
+lines(Day_of_year,predict1$fit-qt(0.975,predict1$df)*predict1$se, lty=2, col="red")
+lines(Day_of_year,predict1$fit+qt(0.975,predict1$df)*predict1$se, lty=2, col="red")
 
 dettach(mydata)
